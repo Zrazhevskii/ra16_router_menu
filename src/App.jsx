59877f04e39a685f4dc71807078bbf5835b424ef
Zrazhevskii <vbn-134@yandex.ru>
2023-12-12 14:38:16 +0300

    создание pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import Menu from './components/Menu'
-import HomePage from './components/HomePage'
-import DriftPage from './components/DriftPage'
-import ForzaPage from './components/ForzaPage'
-import TimeAttackPage from './components/TimeAttackPage'
-import './style/index.css'
-
+import { Routes, Route } from 'react-router-dom';
+import Menu from './components/Menu';
+import HomePage from './pages/HomePage';
+import DriftPage from './pages/DriftPage';
+import ForzaPage from './pages/ForzaPage';
+import TimeAttackPage from './pages/TimeAttackPage';
+// import './style/index.css';
 
 export default function App() {
-  return (
-    <Router>
-      <div>
-        <Menu />
-        <div className="page">
-          <Route path="/" exact component={HomePage} />
-          <Route path="/drift" component={DriftPage} />
-          <Route path="/timeattack" component={TimeAttackPage} />
-          <Route path="/forza" component={ForzaPage} />
+    return (
+        <div>
+            <Routes>
+                <Route path='/' element={<Menu/>}>
+                    <Route index element={<HomePage/>} />
+                    <Route path='/drift' element={<DriftPage/>} />
+                    <Route path='/timeattack' element={<TimeAttackPage/>} />
+                    <Route path='/forza' element={<ForzaPage/>} />
+                    <Route path='*' element={<HomePage/>} />
+                </Route>
+            </Routes>
         </div>
-      </div>
-    </Router>
-  );
+    );
 }
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,3 +1,18 @@
+import { NavLink, Link, Outlet } from 'react-router-dom'
+import '../style/index.css'
+
 export default function Menu() {
-    return null;
+
+  // const activClass = (({isActive}) => (isActive) ? 'menu__item-active' : 'menu__item') ;
+    return (
+      <>
+      <div className='page'>
+        <Link className='menu__item-active' to="/">Главная</Link>
+      </div>
+        <Outlet/>
+        
+        <footer>2021</footer>
+      </>
+      
+    )
   }
